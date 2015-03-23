f7a233725d4b4db8dceccb2f73278ad32829d3a7
novichkov <novichkovv@bk.ru>
2015-03-23 15:31:38 +0300

    у Signed-off-by: novichkov <novichkovv@bk.ru>

--- www/reports/index.php
+++ www/reports/index.php
@@ -7,7 +7,7 @@
  */
 session_start();
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_DIR', realpath($_SERVER['DOCUMENT_ROOT']) . DS);
+define('ROOT_DIR', realpath($_SERVER['DOCUMENT_ROOT']) . DS . 'reports' . DS);
 define('SITE_DIR', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 define('SH_ROOT_DIR', str_replace('reports' . DS, '', ROOT_DIR));
 define('R_SITE_DIR', SITE_DIR . 'reports/');
